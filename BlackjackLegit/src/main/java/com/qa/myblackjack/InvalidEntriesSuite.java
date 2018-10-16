c4d28791d69adb4b59a046132deb7d83f5232479
ssb91 <sasha.birdi@yahoo.co.uk>
2018-10-16 10:12:29 +0100

    ses

---
+++ BlackjackLegit/src/main/java/com/qa/myblackjack/InvalidEntriesSuite.java
@@ -0,0 +1,11 @@
+package com.qa.myblackjack;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ BustBlackjack.class, ClassesBlackjack.class })
+public class InvalidEntriesSuite {
+
+}
