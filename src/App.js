3678541c95658e25de0fa64c5804619550af5305
MehulSaluja <44192569+MehulSaluja@users.noreply.github.com>
2018-10-26 16:12:35 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+import ButtonPanel from "./ButtonPanel";
+
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+  }
+
+
+
+  render() {
+    return (
+      <div className="component-app">
+        
+      </div>
+    );
+  }
+}
+export default App;
